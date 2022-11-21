2b92636067a9bafbbc36608ff1ef2be602258bb1
itganeshtech <it.ganesh.tech@gmail.com>
2022-11-21 10:05:39 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Nov22_Fundamental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("\nProgrma for adding three numbers");
+            int sum = 0;
+            for (int i = 1; i <= 3; i++)
+            {
+                //Console.Write("Enter " + i + " Number\t: ");
+                Console.Write("Enter {0} Number ", i);
+                sum = sum+Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine(sum.ToString());
+        }
+    }
+}
